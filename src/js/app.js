33760f3ab98e1edc64a1cdf078c7715d4289f627
Kalin Atanasov <kalin.natanasov@gmail.com>
2023-01-10 22:14:57 +0200

    task-53:add data attribute

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+var productsCnt=document.querySelectorAll('.product .price').length;
+for(var i=0;i<productsCnt; i++){
+  var price=document.querySelectorAll('.product .price')[i].textContent;
+  var currentEl=document.querySelectorAll('.product')[i];
+  currentEl.dataset.price=price;
+}
 
 });
